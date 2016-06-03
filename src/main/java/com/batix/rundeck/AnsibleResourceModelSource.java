3a771afb085a17741ae6018695a8a5a9cd4d082a
David Kirstein <dak@batix.com>
2016-06-03 23:19:25 +0200

    log error if node discovery fails (e.g. Ansible can't write to fact cache)

--- src/main/java/com/batix/rundeck/AnsibleResourceModelSource.java
+++ src/main/java/com/batix/rundeck/AnsibleResourceModelSource.java
@@ -10,6 +10,7 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.*;
@@ -53,174 +54,179 @@ public class AnsibleResourceModelSource implements ResourceModelSource {
       runner.limit(limit);
     }
 
-    List<String> args = new ArrayList<>();
-    args.add("-e facts=" + (gatherFacts ? "True" : "False"));
-    args.add("-e tmpdir='" + tempDirectory.toFile().getAbsolutePath() + "'");
+    StringBuilder args = new StringBuilder();
+    args.append("-e facts=").append(gatherFacts ? "True" : "False");
+    args.append(" ").append("-e tmpdir='").append(tempDirectory.toFile().getAbsolutePath()).append("'");
     if (extraArgs != null && extraArgs.length() > 0) {
-      args.add(extraArgs);
+      args.append(" ").append(extraArgs);
     }
-    runner.extraArgs(args);
+    runner.extraArgs(args.toString());
 
     try {
-      runner.run();
+      int status = runner.run();
+      if (status != 0) {
+        System.out.println("error gathering facts:\n" + runner.getOutput());
+      }
     } catch (Exception e) {
       throw new ResourceModelSourceException("Error running playbook.", e);
     }
 
     try {
-      DirectoryStream<Path> directoryStream = Files.newDirectoryStream(tempDirectory.resolve("data"));
-      for (Path factFile : directoryStream) {
-        NodeEntryImpl node = new NodeEntryImpl();
+      if (new File(tempDirectory.toFile(), "data").exists()) {
+        DirectoryStream<Path> directoryStream = Files.newDirectoryStream(tempDirectory.resolve("data"));
+        for (Path factFile : directoryStream) {
+          NodeEntryImpl node = new NodeEntryImpl();
 
-        BufferedReader bufferedReader = Files.newBufferedReader(factFile, Charset.forName("utf-8"));
-        JsonElement json = new JsonParser().parse(bufferedReader);
-        bufferedReader.close();
-        JsonObject root = json.getAsJsonObject();
+          BufferedReader bufferedReader = Files.newBufferedReader(factFile, Charset.forName("utf-8"));
+          JsonElement json = new JsonParser().parse(bufferedReader);
+          bufferedReader.close();
+          JsonObject root = json.getAsJsonObject();
 
-        String hostname = root.get("inventory_hostname").getAsString();
-        if (root.has("ansible_host")) {
-          hostname = root.get("ansible_host").getAsString();
-        } else if (root.has("ansible_ssh_host")) { // deprecated variable
-          hostname = root.get("ansible_ssh_host").getAsString();
-        }
-
-        String nodename = root.get("inventory_hostname").getAsString();
-
-        node.setHostname(hostname);
-        node.setNodename(nodename);
-
-        String username = System.getProperty("user.name"); // TODO better default?
-        if (root.has("ansible_user")) {
-          username = root.get("ansible_user").getAsString();
-        } else if (root.has("ansible_ssh_user")) { // deprecated variable
-          username = root.get("ansible_ssh_user").getAsString();
-        } else if (root.has("ansible_user_id")) { // fact
-          username = root.get("ansible_user_id").getAsString();
-        }
-        node.setUsername(username);
-
-        HashSet<String> tags = new HashSet<>();
-        for (JsonElement ele : root.getAsJsonArray("group_names")) {
-          tags.add(ele.getAsString());
-        }
-        node.setTags(tags);
-
-        if (root.has("ansible_lsb")) {
-          node.setDescription(root.getAsJsonObject("ansible_lsb").get("description").getAsString());
-        } else {
-          StringBuilder sb = new StringBuilder();
-
-          if (root.has("ansible_distribution")) {
-            sb.append(root.get("ansible_distribution").getAsString()).append(" ");
-          }
-          if (root.has("ansible_distribution_version")) {
-            sb.append(root.get("ansible_distribution_version").getAsString()).append(" ");
+          String hostname = root.get("inventory_hostname").getAsString();
+          if (root.has("ansible_host")) {
+            hostname = root.get("ansible_host").getAsString();
+          } else if (root.has("ansible_ssh_host")) { // deprecated variable
+            hostname = root.get("ansible_ssh_host").getAsString();
           }
 
-          if (sb.length() > 0) {
-            node.setDescription(sb.toString().trim());
+          String nodename = root.get("inventory_hostname").getAsString();
+
+          node.setHostname(hostname);
+          node.setNodename(nodename);
+
+          String username = System.getProperty("user.name"); // TODO better default?
+          if (root.has("ansible_user")) {
+            username = root.get("ansible_user").getAsString();
+          } else if (root.has("ansible_ssh_user")) { // deprecated variable
+            username = root.get("ansible_ssh_user").getAsString();
+          } else if (root.has("ansible_user_id")) { // fact
+            username = root.get("ansible_user_id").getAsString();
           }
-        }
+          node.setUsername(username);
 
-        // ansible_system     = Linux   = osFamily in Rundeck
-        // ansible_os_family  = Debian  = osName in Rundeck
+          HashSet<String> tags = new HashSet<>();
+          for (JsonElement ele : root.getAsJsonArray("group_names")) {
+            tags.add(ele.getAsString());
+          }
+          node.setTags(tags);
 
-        if (root.has("ansible_system")) {
-          node.setOsFamily(root.get("ansible_system").getAsString());
-        }
+          if (root.has("ansible_lsb")) {
+            node.setDescription(root.getAsJsonObject("ansible_lsb").get("description").getAsString());
+          } else {
+            StringBuilder sb = new StringBuilder();
 
-        if (root.has("ansible_os_family")) {
-          node.setOsName(root.get("ansible_os_family").getAsString());
-        }
+            if (root.has("ansible_distribution")) {
+              sb.append(root.get("ansible_distribution").getAsString()).append(" ");
+            }
+            if (root.has("ansible_distribution_version")) {
+              sb.append(root.get("ansible_distribution_version").getAsString()).append(" ");
+            }
 
-        if (root.has("ansible_architecture")) {
-          node.setOsArch(root.get("ansible_architecture").getAsString());
-        }
+            if (sb.length() > 0) {
+              node.setDescription(sb.toString().trim());
+            }
+          }
 
-        if (root.has("ansible_kernel")) {
-          node.setOsVersion(root.get("ansible_kernel").getAsString());
-        }
+          // ansible_system     = Linux   = osFamily in Rundeck
+          // ansible_os_family  = Debian  = osName in Rundeck
 
-        // JSON-Path -> Attribute-Name
-        Map<String, String> interestingItems = new HashMap<>();
+          if (root.has("ansible_system")) {
+            node.setOsFamily(root.get("ansible_system").getAsString());
+          }
 
-        interestingItems.put("ansible_form_factor", "form_factor");
+          if (root.has("ansible_os_family")) {
+            node.setOsName(root.get("ansible_os_family").getAsString());
+          }
 
-        interestingItems.put("ansible_system_vendor", "system_vendor");
+          if (root.has("ansible_architecture")) {
+            node.setOsArch(root.get("ansible_architecture").getAsString());
+          }
 
-        interestingItems.put("ansible_product_name", "product_name");
-        interestingItems.put("ansible_product_version", "product_version");
-        interestingItems.put("ansible_product_serial", "product_serial");
+          if (root.has("ansible_kernel")) {
+            node.setOsVersion(root.get("ansible_kernel").getAsString());
+          }
 
-        interestingItems.put("ansible_bios_version", "bios_version");
-        interestingItems.put("ansible_bios_date", "bios_date");
+          // JSON-Path -> Attribute-Name
+          Map<String, String> interestingItems = new HashMap<>();
 
-        interestingItems.put("ansible_machine_id", "machine_id");
+          interestingItems.put("ansible_form_factor", "form_factor");
 
-        interestingItems.put("ansible_virtualization_type", "virtualization_type");
-        interestingItems.put("ansible_virtualization_role", "virtualization_role");
+          interestingItems.put("ansible_system_vendor", "system_vendor");
 
-        interestingItems.put("ansible_selinux", "selinux");
-        interestingItems.put("ansible_fips", "fips");
+          interestingItems.put("ansible_product_name", "product_name");
+          interestingItems.put("ansible_product_version", "product_version");
+          interestingItems.put("ansible_product_serial", "product_serial");
 
-        interestingItems.put("ansible_service_mgr", "service_mgr");
-        interestingItems.put("ansible_pkg_mgr", "pkg_mgr");
+          interestingItems.put("ansible_bios_version", "bios_version");
+          interestingItems.put("ansible_bios_date", "bios_date");
 
-        interestingItems.put("ansible_distribution", "distribution");
-        interestingItems.put("ansible_distribution_version", "distribution_version");
-        interestingItems.put("ansible_distribution_major_version", "distribution_major_version");
-        interestingItems.put("ansible_distribution_release", "distribution_release");
-        interestingItems.put("ansible_lsb.codename", "lsb_codename");
+          interestingItems.put("ansible_machine_id", "machine_id");
 
-        interestingItems.put("ansible_domain", "domain");
+          interestingItems.put("ansible_virtualization_type", "virtualization_type");
+          interestingItems.put("ansible_virtualization_role", "virtualization_role");
 
-        interestingItems.put("ansible_date_time.tz", "tz");
-        interestingItems.put("ansible_date_time.tz_offset", "tz_offset");
+          interestingItems.put("ansible_selinux", "selinux");
+          interestingItems.put("ansible_fips", "fips");
 
-        interestingItems.put("ansible_processor_count", "processor_count");
-        interestingItems.put("ansible_processor_cores", "processor_cores");
-        interestingItems.put("ansible_processor_vcpus", "processor_vcpus");
-        interestingItems.put("ansible_processor_threads_per_core", "processor_threads_per_core");
+          interestingItems.put("ansible_service_mgr", "service_mgr");
+          interestingItems.put("ansible_pkg_mgr", "pkg_mgr");
 
-        interestingItems.put("ansible_userspace_architecture", "userspace_architecture");
-        interestingItems.put("ansible_userspace_bits", "userspace_bits");
+          interestingItems.put("ansible_distribution", "distribution");
+          interestingItems.put("ansible_distribution_version", "distribution_version");
+          interestingItems.put("ansible_distribution_major_version", "distribution_major_version");
+          interestingItems.put("ansible_distribution_release", "distribution_release");
+          interestingItems.put("ansible_lsb.codename", "lsb_codename");
 
-        interestingItems.put("ansible_memtotal_mb", "memtotal_mb");
-        interestingItems.put("ansible_swaptotal_mb", "swaptotal_mb");
-        interestingItems.put("ansible_processor.0", "processor0");
-        interestingItems.put("ansible_processor.1", "processor1");
+          interestingItems.put("ansible_domain", "domain");
 
-        for (Map.Entry<String, String> item : interestingItems.entrySet()) {
-          String[] itemParts = item.getKey().split("\\.");
+          interestingItems.put("ansible_date_time.tz", "tz");
+          interestingItems.put("ansible_date_time.tz_offset", "tz_offset");
 
-          if (itemParts.length > 1) {
-            JsonElement ele = root;
-            for (String itemPart : itemParts) {
-              if (ele.isJsonArray() && itemPart.matches("^\\d+$") && ele.getAsJsonArray().size() > Integer.parseInt(itemPart)) {
-                ele = ele.getAsJsonArray().get(Integer.parseInt(itemPart));
-              } else if (ele.isJsonObject() && ele.getAsJsonObject().has(itemPart)) {
-                ele = ele.getAsJsonObject().get(itemPart);
-              } else {
-                ele = null;
-                break;
+          interestingItems.put("ansible_processor_count", "processor_count");
+          interestingItems.put("ansible_processor_cores", "processor_cores");
+          interestingItems.put("ansible_processor_vcpus", "processor_vcpus");
+          interestingItems.put("ansible_processor_threads_per_core", "processor_threads_per_core");
+
+          interestingItems.put("ansible_userspace_architecture", "userspace_architecture");
+          interestingItems.put("ansible_userspace_bits", "userspace_bits");
+
+          interestingItems.put("ansible_memtotal_mb", "memtotal_mb");
+          interestingItems.put("ansible_swaptotal_mb", "swaptotal_mb");
+          interestingItems.put("ansible_processor.0", "processor0");
+          interestingItems.put("ansible_processor.1", "processor1");
+
+          for (Map.Entry<String, String> item : interestingItems.entrySet()) {
+            String[] itemParts = item.getKey().split("\\.");
+
+            if (itemParts.length > 1) {
+              JsonElement ele = root;
+              for (String itemPart : itemParts) {
+                if (ele.isJsonArray() && itemPart.matches("^\\d+$") && ele.getAsJsonArray().size() > Integer.parseInt(itemPart)) {
+                  ele = ele.getAsJsonArray().get(Integer.parseInt(itemPart));
+                } else if (ele.isJsonObject() && ele.getAsJsonObject().has(itemPart)) {
+                  ele = ele.getAsJsonObject().get(itemPart);
+                } else {
+                  ele = null;
+                  break;
+                }
+              }
+
+              if (ele != null && ele.isJsonPrimitive() && ele.getAsString().length() > 0) {
+                node.setAttribute(item.getValue(), ele.getAsString());
+              }
+            } else {
+              if (root.has(item.getKey())
+                && root.get(item.getKey()).isJsonPrimitive()
+                && root.get(item.getKey()).getAsString().length() > 0) {
+                node.setAttribute(item.getValue(), root.get(item.getKey()).getAsString());
               }
             }
-
-            if (ele != null && ele.isJsonPrimitive() && ele.getAsString().length() > 0) {
-              node.setAttribute(item.getValue(), ele.getAsString());
-            }
-          } else {
-            if (root.has(item.getKey())
-              && root.get(item.getKey()).isJsonPrimitive()
-              && root.get(item.getKey()).getAsString().length() > 0) {
-              node.setAttribute(item.getValue(), root.get(item.getKey()).getAsString());
-            }
           }
-        }
 
-        nodes.putNode(node);
+          nodes.putNode(node);
+        }
+        directoryStream.close();
       }
-      directoryStream.close();
     } catch (IOException e) {
       throw new ResourceModelSourceException("Error reading facts.", e);
     }
