e520395a6c0c34fc46a5212e0028d8241754d74e
Michael Cassaniti <mcassaniti@csa.com.au>
2017-11-23 18:38:04 +1100

    Added additional import "java.nio.file.Paths" for AnsibleRunner

--- src/main/java/com/batix/rundeck/core/AnsibleRunner.java
+++ src/main/java/com/batix/rundeck/core/AnsibleRunner.java
@@ -10,6 +10,7 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.attribute.PosixFilePermission;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
